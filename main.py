d486f9a2342b3b98fcb092e023dcb8142e8de761
nijigen-plot <quark_hardcore@yahoo.co.jp>
2022-07-10 17:11:42 +0900

    ファイル同期をテスト

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
     target = search_upload_files.search("sql_template")
     GDS = googleDriveSync(gd_target_folder_id=GOOGLE_DRIVE_TARGET_ID, upload_target_data=target)
     GDS.folder_exist_check()
+    GDS.sync()
 
 
 if __name__ == "__main__":
