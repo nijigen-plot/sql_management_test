060385587780c052f4781c8efb3c9e8c9355c534
nijigen-plot <quark_hardcore@yahoo.co.jp>
2022-07-10 20:34:17 +0900

    実行時ファイルが同期するように変更

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ GOOGLE_DRIVE_TARGET_ID = "1CpEDoL_vLWibMKar2pFsGAY-F-MSJ9P7"
 
 
 def main():
+    # sql_template内のファイルを同期する
     target = search_upload_files.search("sql_template")
     GDS = googleDriveSync(gd_target_folder_id=GOOGLE_DRIVE_TARGET_ID, upload_target_data=target)
-    GDS.folder_exist_check()
     GDS.sync()
+    # 指定フォルダ内のファイルを消したいだけの場合以下を実行する
+    # GDS.file_delete()
 
 
 if __name__ == "__main__":
