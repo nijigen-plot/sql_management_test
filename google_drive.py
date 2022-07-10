c87863777e026a17787d7dc272e240165a9d3b35
nijigen-plot <quark_hardcore@yahoo.co.jp>
2022-07-10 16:50:01 +0900

    WIP sqlファイルメソッドを作成

--- google_drive.py
+++ google_drive.py
@@ -4,12 +4,15 @@ import os
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
 
 
 class googleDriveSync:
-    def __init__(self, gd_target_folder_id: str):
-        # 対象のGoogle DriveフォルダID
+    def __init__(self, gd_target_folder_id: str, upload_target_data: list):
+        # ファイル配置対象のGoogle DriveフォルダID
         self.sync_folder_id = gd_target_folder_id
+        # アップロード対象のデータ群ディレクトリリスト
+        self.upload_target_data = upload_target_data
         # 環境変数からcredentials情報を作成
         target_env = os.environ.get("GCP_SERVICE_ACCOUNT_KEY")
         if target_env == None:
@@ -34,6 +37,7 @@ class googleDriveSync:
         items = results.get("files", [])
         if not items:
             print("Folder not found.")
+            return
         file_exist = False
         for item in items:
             if item["id"] == self.sync_folder_id:
@@ -44,3 +48,47 @@ class googleDriveSync:
         else:
             print("Folder not found.")
             return
+
+    # フォルダを作成する
+    def create_folder(self, folder_name: str):
+        try:
+            file_metadata = {"title": folder_name, "mimeType": "application/vnd.google-apps.folder"}
+
+            file = self.service.files().create(body=file_metadata, fields="id").execute()
+
+            print(f'Folder has created with ID: "{file.get("id")}".')
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+            file = None
+        return file.get("id")
+
+    # ファイルを作成する
+    def create_file(self, file_name: str, parent_id=None):
+        # parend_idの指定がない場合は、指定フォルダ直下
+        if parent_id == None:
+            parent_id = self.gd_target_folder_id
+        try:
+            file_metadata = {"name": file_name, "parents": [parent_id]}
+            media = MediaFileUpload(file_name, minetype="text/x-sql", resumable=True)
+            file = self.service.files().create(body=file_metadata, media_body=media, fields="id").execute()
+            print(f'File with ID: "{file.get("id")}" has added to the folder with ' f'ID "{parent_id}".')
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+            file = None
+
+    # githubのファイル構造のままsqlファイルをアップロードする
+    def create_flow(self, directory: str):
+        split_directory = directory.split("/")
+        directory_length = len(split_directory)
+        parent_id = None
+        for i in range(directory_length):
+            # directory_lengthよりもiが小さい場合、その名前でフォルダを作成する
+            if i + 1 < directory_length:
+                parent_id = self.create_folder(folder_name=split_directory[i])
+            else:
+                self.create_file(file_name=split_directory[i], parent_id=parent_id)
+
+    # sqlファイルを同期する
+    def sync(self):
+        for target_directory in self.upload_target_data:
+            self.create_flow(target_directory)
