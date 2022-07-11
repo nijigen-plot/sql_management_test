d85fb2c1b8f2c10b71947677a5d0ce5ed7de7406
nijigen-plot <quark_hardcore@yahoo.co.jp>
2022-07-12 01:05:04 +0900

    ディレクトリ操作を追加 search_upload_files.pyの関数を移動

--- search_upload_files.py
+++
@@ -1,8 +0,0 @@
-# ルートディレクトリからの実行を想定しています。
-import os
-import pathlib
-
-
-def search(target_directory: str):
-    os.chdir(target_directory)
-    return list(map(lambda x: str(x), list(pathlib.Path(".").glob("**/*.sql"))))
